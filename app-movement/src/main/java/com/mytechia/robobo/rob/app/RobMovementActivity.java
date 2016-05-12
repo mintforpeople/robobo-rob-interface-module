3785e89dbba717e6622a16fe9fe5d6dd531a215c
Gervasio Varela Fernández <gervasio.varela@mytechia.com>
2016-05-12 16:59:14 +0200

    Added LGPL license comment to the code.

--- app-movement/src/main/java/com/mytechia/robobo/rob/app/RobMovementActivity.java
+++ app-movement/src/main/java/com/mytechia/robobo/rob/app/RobMovementActivity.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ *
+ *   Copyright 2016 Mytech Ingenieria Aplicada <http://www.mytechia.com>
+ *   Copyright 2016 Gervasio Varela <gervasio.varela@mytechia.com>
+ *
+ *   This file is part of Robobo Framework Library.
+ *
+ *   Robobo Framework Library is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Robobo Framework Library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with Robobo Framework Library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
 package com.mytechia.robobo.rob.app;
 
 import android.app.Activity;
@@ -26,6 +48,10 @@ import com.mytechia.robobo.rob.movement.IRobMovementModule;
 import java.util.Collection;
 import java.util.Date;
 
+/** Custom Robobo display activty that provide control to move the ROB
+ *
+ * @author Gervasio Varela
+ */
 public class RobMovementActivity extends Activity {
 
     private FrameworkManager roboboManager;
